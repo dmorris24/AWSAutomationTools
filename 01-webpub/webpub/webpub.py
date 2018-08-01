5c8f9c0954e73f6e6283cf73892eb018be0456af
Derrick Morris <dmorris@nveg.com>
2018-08-01 17:16:52 -0400

    webpub folder and files

---
+++ 01-webpub/webpub/webpub.py
@@ -0,0 +1,36 @@
+import boto3
+import click
+
+session = boto3.Session()
+s3 = session.resource('s3')
+
+
+@click.group()
+def cli():
+  "Deploys websites to AWS"
+  pass
+
+
+@cli.command('list-buckets')
+def list_buckets():
+  '''List all s3 buckets'''
+  for bucket in s3.buckets.all():
+    print(bucket)
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+  "List of contents in bucket"
+  for obj in s3.Bucket(bucket).objects.all():
+    print(obj)
+
+@cli.command('runGab')
+def run_Gab():
+  '''prints Greeting to Gab'''
+  print("Hello Gabriella, How's your day going?")
+
+if __name__ == '__main__':
+  cli()
+
+  
+
